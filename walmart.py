1af04341815c48aa248c3762e69801d1efcdcf81
igor <igoralann113@gmail.com>
2025-04-05 03:06:29 +0900

    update walmart

--- walmart.py
+++ walmart.py
@@ -236,7 +236,7 @@ def get_product_list(driver, db_name, table_name, current_time, prefix):
             products.append(record)
             print(record)
             section_id = section_id + 1
-            if(section_id > 100):
+            if(section_id > 50):
                 break
         num = num + 1
         break
